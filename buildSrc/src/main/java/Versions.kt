e52895651f2415071d3f21000f55be5393340ace
Pavlo Rekun <rekunpavlo@gmail.com>
2022-06-27 16:43:06 +0300

    - Updated: Italian localization. - Updated: Gradle dependencies. - Updated: Atmosphere payload.

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,20 +1,20 @@
 object Versions {
 
     object Gradle {
-        const val Plugin = "7.1.3"
+        const val Plugin = "7.2.1"
         const val Updates = "0.42.0"
     }
 
     object AndroidX {
-        const val Core = "1.7.0"
+        const val Core = "1.8.0"
         const val RecyclerView = "1.2.1"
         const val Browser = "1.4.0"
         const val Preferences = "1.2.0"
-        const val ConstraintLayout = "2.1.3"
+        const val ConstraintLayout = "2.1.4"
         const val Lifecycle = "2.4.1"
         const val Navigation = "2.4.2"
         const val SwipeRefreshLayout = "1.1.0"
-        const val Hilt = "2.41"
+        const val Hilt = "2.42"
     }
 
     object Design {
@@ -27,12 +27,12 @@ object Versions {
         const val Moshi = "1.13.0"
         const val Retrofit = "2.9.0"
         const val Zxing = "4.3.0"
-        const val OkHttpLogging = "4.9.3"
+        const val OkHttpLogging = "4.10.0"
     }
 
     object Kotlin {
-        const val Kotlin = "1.6.20"
-        const val Coroutines = "1.6.1"
+        const val Kotlin = "1.7.0"
+        const val Coroutines = "1.6.3"
     }
 
     object Other {
